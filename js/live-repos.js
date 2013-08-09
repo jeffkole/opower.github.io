e10560bf955eda85bbffbb74ec98d4a89deafcee
Jeff Kolesky <jeff@kolesky.com>
2013-08-08 21:31:47 -0700

    Adds more safety around null data

--- js/live-repos.js
+++ js/live-repos.js
@@ -3,20 +3,22 @@ $(document).ready(function() {
         var project = $(this);
         var repo = project.attr("repo");
         $.getJSON("https://api.github.com/repos/" + repo, function(data) {
-            var author = "";
-            if (data.owner.login != "opower") {
-                author = '<div class="author"><a href="' + data.owner.html_url + '">' + data.owner.login + '</a></div>';
+            var html = '<h4><a href="' + data.html_url + '">' + data.name + '</a></h4>';
+            if (data.description) {
+                html += '<div class="description">' + data.description + '</div>';
             }
-            project.append(
-                '<h4><a href="' + data.html_url + '">' + data.name + '</a></h4>' +
-                '<div class="description">' + data.description + '</div>' +
-                '<div class="stats">' +
-                    author +
-                    '<div class="language">' + data.language + '</div>' +
-                    '<div class="updatedon">Updated on ' + new Date(data.updated_at).toLocaleDateString() + '</div>' +
+            html += '<div class="stats">';
+            if (data.owner.login != "opower") {
+                html += '<div class="author"><a href="' + data.owner.html_url + '">' + data.owner.login + '</a></div>';
+            }
+            if (data.language) {
+                html += '<div class="language">' + data.language + '</div>';
+            }
+            html += '<div class="updatedon">Updated on ' + new Date(data.updated_at).toLocaleDateString() + '</div>' +
                     '<div class="forks">' + data.forks_count + ' forks</div>' +
                     '<div class="watchers">' + data.watchers_count + ' watchers</div>' +
-                '</div>');
+                    '</div>';
+            project.append(html);
         });
     });
 });
